e7ced698b99536f4bf2058f7af3f7e1401c4d939
Hannah Tops <hannahtops@gmail.com>
2015-11-15 15:04:00 +0100

    Generates all options for an n by n matrix in a list.

--- MatrixGenerator.py
+++ MatrixGenerator.py
@@ -18,18 +18,30 @@ class MatrixGenerator:
 		for k,v in d.items():
 	 		p = [i for i in p if i[k] not in v]		
 		return p
-
+	def opt(self, p, his = []):
+		global alles
+		for a in p:
+			a = [a] + his
+			aOpt = self.poss(a)
+			if len(a) == self.n-1:
+				alles.append(a + aOpt)
+			else:
+				self.opt(aOpt,a)
 	def optionalMatrices(self):
 		p = self.perm()
-		a = next(p)
-		rOpt = self.poss([a])
-		for r in rOpt:
-			b = [a] + [r]
-			optB = self.poss(b)
-			for B in optB:
-				c = [a] + [r] + [B]
-				optC = self.poss(c)
-				print c,optC
+		self.opt(p)
+#		for a in p:
+#			a = [a]
+#			aOpt = self.poss(a)
+#			for A in aOpt:
+#				b = a + [A]
+#				optB = self.poss(b)
+#				for B in optB:
+#					c = b + [B]
+#					optC = self.poss(c)
+#					print c+optC
 
-m = MatrixGenerator(4)
+alles = []
+m = MatrixGenerator(3)
 m.optionalMatrices()
+print alles
